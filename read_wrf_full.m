cfcc3beb24b569288e46989469841158ad36c566
Bonnie Brown <brbrown@santaana.soest.hawaii.edu>
2014-10-31 13:31:03 -1000

    Initial commit read_wrf_full.m Reads a 3D field from WRF NetCDF output given a file path and name, variable name, starting time index, and time index interval. Detects if a field is staggered and unstaggers to mass points. For use with "new" NetCDF libraries in MATLAB, tested in version R2014a.

---
+++ read_wrf_full.m
@@ -0,0 +1,35 @@
+function [field] = read_wrf_full(pth,fl,vnam,ti,int)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% read_wrf_full.m
+%
+% read a full, 3-D field from WRF wrfout file for a time interval and unstagger to mass points
+% if necessary.
+%
+%       Bonnie Brown, University of Hawaii
+%           October 2014
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ncfile = strcat(pth,fl);
+
+% get variable at desired times
+dat = ncread(ncfile,vnam,[1 1 1 ti],[inf inf inf int]);
+[we,sn,neta] = size(dat);
+
+% get size of unstaggered grid
+weu = ncreadatt(ncfile,'/','WEST-EAST_PATCH_END_UNSTAG');
+snu = ncreadatt(ncfile,'/','SOUTH-NORTH_PATCH_END_UNSTAG');
+nulev = ncreadatt(ncfile,'/','BOTTOM-TOP_PATCH_END_UNSTAG');
+
+% unstagger if necessary 
+if we>weu
+    field = 0.5*(dat(2:end,:,:) + dat(1:end-1,:,:));
+elseif sn>snu
+    field = 0.5*(dat(:,2:end,:) + dat(:,1:end-1,:));
+elseif neta>nulev
+    field = 0.5*(dat(:,:,2:end) + dat(:,:,1:end-1));
+else 
+    field = dat;
+end
